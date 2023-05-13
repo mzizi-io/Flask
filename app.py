19e7f9233af064e59e3d94e126f7c8084734de4b
Caleb Migosi <calebmigosi@gmail.com>
2023-05-13 16:58:12 +0200

    Removed error pages

--- app.py
+++ app.py
@@ -135,19 +135,6 @@ def contact():
                             message = message, 
                             form = form))
 
-
-"ERROR PAGES"
-"=========================="
-# Error 404
-@app.errorhandler(404)
-def page_not_found(e):
-    return (render_template("404.html"), 404)
-
-# Error 500
-@app.errorhandler(500)
-def page_not_found(e):
-    return (render_template("404.html"),500)
- 
 if __name__ == "__main__":
     app.run()
 
